f59cf5ac5a01be90fa9884e0b80dba8dd6cba52a
Ryan Ransford <rdransfo@gmail.com>
2012-04-19 23:35:34 -0500

    refactor: cyclomatic complexity <8

--- src/main/java/com/blogspot/activeactive/kata/BankOcr.java
+++ src/main/java/com/blogspot/activeactive/kata/BankOcr.java
@@ -5,28 +5,13 @@ import java.util.ArrayList;
 
 public class BankOcr {
   public List<String[]> parseLines(final List<String> lines) {
-    if (lines == null || lines.isEmpty()) {
-      throw new IllegalArgumentException("The lines parameter is required and"
-          + " must not be null-valued or empty.");
-    }
-    if ((lines.size() % 4) != 0) {
-      throw new IllegalArgumentException("Each valid entry in the lines"
-          + " parameter must contain exactly four lines. The lines parameter"
-          + " does not contain a whole number of valid entries.");
-    }
+    validateLines(lines);
+
     final List<String[]> result = new ArrayList<String[]>();
     int lineIdx = 0;
     String[] entry = new String[4];
     for (final String line : lines) {
-      if (line.length() != 27) {
-        throw new IllegalArgumentException("Each valid line contained in the"
-            + " lines parameter must contain exactly 27 characters.");
-      }
-      if (! line.replaceAll("[|_ ]", "").equals("")) {
-        throw new IllegalArgumentException("Each valid line contained in the"
-            + " lines parameter may only contain underscore (\"_\")"
-            + " characters, pipe (\"|\") characters, or spaces (\" \").");
-      }
+      validateLine(line);
       entry[lineIdx] = line;
       if (lineIdx == 3) {
         lineIdx = 0;
@@ -37,5 +22,29 @@ public class BankOcr {
       }
     }
     return result;
+  }
+
+  private void validateLine(final String line) {
+    if (line.length() != 27) {
+      throw new IllegalArgumentException("Each valid line contained in the"
+          + " lines parameter must contain exactly 27 characters.");
+    }
+    if (! line.replaceAll("[|_ ]", "").equals("")) {
+      throw new IllegalArgumentException("Each valid line contained in the"
+          + " lines parameter may only contain underscore (\"_\")"
+          + " characters, pipe (\"|\") characters, or spaces (\" \").");
+    }
+  }
+
+  private void validateLines(final List<String> lines) {
+    if (lines == null || lines.isEmpty()) {
+      throw new IllegalArgumentException("The lines parameter is required and"
+          + " must not be null-valued or empty.");
+    }
+    if ((lines.size() % 4) != 0) {
+      throw new IllegalArgumentException("Each valid entry in the lines"
+          + " parameter must contain exactly four lines. The lines parameter"
+          + " does not contain a whole number of valid entries.");
+    }
   }
 }
